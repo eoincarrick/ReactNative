4d751fbbef15d12484688144666170b0ce0b74c0
Eoin Carrick <22carri99@gmail.com>
2022-02-12 12:50:52 +0000

    code

--- rn-game-app/App.js
+++ rn-game-app/App.js
@@ -1,8 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { Header } from "react-native/Libraries/NewAppScreen";
 
 export default function App() {
-  return <View></View>;
+  return (
+    <View>
+      <Header />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({});
